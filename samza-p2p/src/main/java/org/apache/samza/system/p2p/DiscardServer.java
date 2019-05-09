3415735deee71d8835c1374c352a53ab9316c571
Prateek Maheshwari <pmaheshwari@apache.org>
2019-05-09 11:00:47 -0700

    Commit

---
+++ samza-p2p/src/main/java/org/apache/samza/system/p2p/DiscardServer.java
@@ -0,0 +1,119 @@
+package org.apache.samza.system.p2p;
+
+import io.netty.bootstrap.ServerBootstrap;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import org.jboss.netty.channel.ChannelFutureListener;
+
+
+/**
+ * Discards any incoming data.
+ */
+public class DiscardServer {
+
+  private int port;
+
+  public DiscardServer(int port) {
+    this.port = port;
+  }
+
+  public void run() throws Exception {
+    EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
+    try {
+      ServerBootstrap b = new ServerBootstrap(); // (2)
+      b.group(bossGroup, workerGroup)
+          .channel(NioServerSocketChannel.class) // (3)
+          .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+            @Override
+            public void initChannel(SocketChannel ch) throws Exception {
+              ch.pipeline().addLast(new DiscardServerHandler());
+            }
+          })
+          .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+          .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+      // Bind and start to accept incoming connections.
+      ChannelFuture f = b.bind(port).sync(); // (7)
+
+      // Wait until the server socket is closed.
+      // In this example, this does not happen, but you can do that to gracefully
+      // shut down your server.
+      f.channel().closeFuture().sync();
+    } finally {
+      workerGroup.shutdownGracefully();
+      bossGroup.shutdownGracefully();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    int port = 8080;
+    if (args.length > 0) {
+      port = Integer.parseInt(args[0]);
+    }
+
+    new DiscardServer(port).run();
+  }
+}
+
+/**
+ * Handles a server-side channel.
+ */
+class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+    ByteBuf in = (ByteBuf) msg;
+    try {
+      while (in.isReadable()) { // (1)
+        System.out.print((char) in.readByte());
+        System.out.flush();
+      }
+    } finally {
+      ReferenceCountUtil.release(msg);
+    }
+  }
+
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+    // Close the connection when an exception is raised.
+    cause.printStackTrace();
+    ctx.close();
+  }
+}
+
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+  @Override
+  public void channelActive(final ChannelHandlerContext ctx) { // (1)
+    final ByteBuf time = ctx.alloc().buffer(4); // (2)
+    time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+
+    final ChannelFuture f = ctx.writeAndFlush(time); // (3)
+    f.addListener(new ChannelFutureListener() {
+      @Override
+      public void operationComplete(ChannelFuture future) {
+        assert f == future;
+        ctx.close();
+      }
+    }); // (4)
+  }
+
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    cause.printStackTrace();
+    ctx.close();
+  }
+}
